2bffa72646ed4a5e0d25f2fbdf612ec65a43768a
Partha Sarathi Kar <partha.sarathi@bdosn.org>
2020-10-31 12:58:58 +0200

    color theme added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MapComponent } from './map/map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'map', component: MapComponent },
+  { path: '', redirectTo: 'map', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
